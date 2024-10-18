2b564d7e29a38754000279e476fbff9f833fbbae
Sam Rayatnia <h.rayatnia@gmail.com>
2024-10-18 17:11:48 +0200

    Initial Commit

---
+++ Hafez/HafezApp.swift
@@ -0,0 +1,17 @@
+//
+//  HafezApp.swift
+//  Hafez
+//
+//  Created by Sam Rayatnia on 18.10.24.
+//
+
+import SwiftUI
+
+@main
+struct HafezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HafezTests/HafezTests.swift
@@ -0,0 +1,16 @@
+//
+//  HafezTests.swift
+//  HafezTests
+//
+//  Created by Sam Rayatnia on 18.10.24.
+//
+
+import Testing
+
+struct HafezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
